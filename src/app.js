a19fcf89301e73180da4323302a37a20c811b031
pramodniroshan <pramodnakandara@gmail.com>
2023-11-24 19:04:48 +0530

    remove all app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,4 @@
 import express from 'express';
-import exampleRoutes from './routes/exampleRoutes.js';
-import genderRoutes from './routes/genderRoutes.js'
-import newsRoutes from './routes/newsRoutes.js'
-import UserRoutes from './routes/userRoutes.js'
-import profilePhotoRoutes from './routes/profilePhotoRoutes.js'
-import profileRoutes from './routes/profileRoutes.js'
-import postRoutes from './routes/postRoutes.js'
-
 import {connectToDatabase} from './database/db.js'
 import {connectToProjectDatabase} from './database/projectdb.js'
 import cors from 'cors';
@@ -50,43 +42,6 @@ async function startServer() {
 startServer();
 
 app.use('/api/auth', authenticateJWT);
-app.use('/api', exampleRoutes);
-app.use('/api', genderRoutes);
-app.use('/api', newsRoutes)
-app.use('/api', UserRoutes);
-app.use('/api', profileRoutes);
-app.use('/api', profilePhotoRoutes);
-app.use('/api', postRoutes);
-
-app.get('/auth/google', (req, res) => {
-  const url = googleOAuth2Client.generateAuthUrl({
-    access_type: 'offline', // Request a refresh token
-    scope: ['openid', 'profile', 'email'], // Requested scopes
-  });
-  res.redirect(url);
-});
-
-
-
-app.get('/auth/google/callback', async (req, res) => {
-  const code = req.query.code;
-
-  try {
-    const { tokens } = await googleOAuth2Client.getToken(code);
-    const accessToken = tokens.access_token;
-    
-    const idToken = tokens.id_token;
-    const refreshToken = tokens.refresh_token;
-
-    console.log(accessToken, 'Access Token');
-    console.log(refreshToken, 'Refresh Token');
-
-    res.status(200).json({ message: 'Successfully authenticated with Google' });
-  } catch (error) {
-    console.error('Error during OAuth 2.0 token exchange:', error);
-    res.status(500).json({ error: 'OAuth 2.0 token exchange failed' });
-  }
-});
 
 app.get('/', (req, res) => {
   res.json({
