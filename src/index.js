94192b8251d683cfe4c0c72bbfefdf90e0ad4e38
pramodniroshan <pramodnakandara@gmail.com>
2023-11-24 21:36:38 +0530

    remove src

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// Importing the 'app' from the ES6 module file './app.js'
-import app from './app.js';
-
-const port = process.env.PORT || 5000;
-
-// Using ES6 arrow function to start the server and log a message
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
