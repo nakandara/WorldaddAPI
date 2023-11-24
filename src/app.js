8e7b167c4ecb70195ba7261adab30b380de9e7bb
pramodniroshan <pramodnakandara@gmail.com>
2023-11-24 22:50:20 +0530

    add example route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
+
 import exampleRoutes from './routes/exampleRoutes.js';
-import genderRoutes from './routes/genderRoutes.js'
-import newsRoutes from './routes/newsRoutes.js'
 import UserRoutes from './routes/userRoutes.js'
 
 import {connectToDatabase} from './database/db.js'
@@ -49,11 +48,8 @@ async function startServer() {
 
   app.use('/api/auth', authenticateJWT);
 
-
-  app.use('/api', exampleRoutes);
-app.use('/api', genderRoutes);
-app.use('/api', newsRoutes)
   app.use('/api', UserRoutes);
+  app.use('/api', exampleRoutes);
 
 app.get('/', (req, res) => {
   res.json({
