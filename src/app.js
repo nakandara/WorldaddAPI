b5b461f8921d37c5de7121d94d340084ffa7c275
pramodniroshan <pramodnakandara@gmail.com>
2023-11-24 18:44:36 +0530

    add after changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
-const express = require('express');
+import express from 'express';
+import {connectToDatabase} from './database/db.js'
+import {connectToProjectDatabase} from './database/projectdb.js'
+import cors from 'cors';
+import { authenticateJWT } from './common/passport.js'
+import { OAuth2Client } from 'google-auth-library';
+
+const googleClientId = "327746728050-s21vp9i9uu5674us70o91cj6qqggs0ib.apps.googleusercontent.com";
+const googleClientSecret = "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";
+const googleRedirectUri = 'http://localhost:8080/auth/google/callback';
+
+const googleOAuth2Client = new OAuth2Client(googleClientId, googleClientSecret, googleRedirectUri);
+
+
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
+async function startServer() {
+  try {
+    // Connect to the database
+    await connectToDatabase();
+    await connectToProjectDatabase();
+
+    // Start your server or perform other initialization tasks
+    // ...
+  } catch (err) {
+    console.error('Error starting the server', err);
+    process.exit(1); // Exit the application on error
+  }
+}
+
+startServer();
+
+app.use('/api/auth', authenticateJWT);
 
 app.get('/', (req, res) => {
   res.json({
@@ -10,6 +40,4 @@ app.get('/', (req, res) => {
   });
 });
 
-
-
-module.exports = app;
+export default app;
---
+++ src/common/passport.js
@@ -0,0 +1,71 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import passport from 'passport';
+import User from '../models/userModel.js';
+import bodyParser from 'body-parser';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import jwt from 'jsonwebtoken';
+
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json({ strict: false }));
+
+const secretKey = process.env.JWT_SECRET || '';
+
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secretKey, // Replace with your actual secret key
+  };
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        // Perform user lookup based on jwt_payload.sub or other criteria
+        const user = await findUserById(jwt_payload.sub);
+  
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      } catch (error) {
+        return done(error, false);
+      }
+    })
+  );
+
+// Middleware for authenticating JWT token
+async function authenticateJWT(req, res, next) {
+    let token = req.headers.authorization?.split(' ')[1];
+
+    if (token) {
+        try {
+            const decodedToken= jwt.verify(token, secretKey);
+            const user = await User.findById(decodedToken.userId);
+            if (!user) {
+                res.status(401).json({ success: false, error: 'Unauthorize' });
+            }
+            req.user = user;
+            next();
+        } catch (error) {
+            
+            res.status(403).json({ success: false, error: 'Forbidden' });
+        }
+    } else {
+        res.status(401).json({ success: false, error: 'Unauthorize' });;
+    }
+}
+
+
+app.use(passport.initialize());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+export { passport, authenticateJWT };
