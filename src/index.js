2ec9d54376a8bc7cbadf046c46d3185f9a6fb8e8
pramodniroshan <pramodnakandara@gmail.com>
2023-11-24 23:05:34 +0530

    add uploade folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // Using ES6 arrow function to start the server and log a message
 app.listen(port, () => {
