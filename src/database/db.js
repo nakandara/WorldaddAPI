c86837ea342d17c4d2461abf30f1256bd7d16161
pramodniroshan <pramodnakandara@gmail.com>
2024-03-29 00:44:41 +0530

    add second space

--- src/database/db.js
+++ src/database/db.js
@@ -7,12 +7,13 @@ const uri = process.env.MONGODG_URI_USERDB;
 
 export const connectToProjectDatabase = async () => {
   try {
-    
+
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to database!');
   } catch (err) {
     console.error('Error connecting to MongoDB', err);
     process.exit(1);
+    
   }
 };
 
