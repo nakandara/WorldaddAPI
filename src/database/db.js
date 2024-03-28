5a120dc5e479c5689c9766ca4d5df222399adebc
pramodniroshan <pramodnakandara@gmail.com>
2024-03-28 23:51:43 +0530

    add space

--- src/database/db.js
+++ src/database/db.js
@@ -7,6 +7,7 @@ const uri = process.env.MONGODG_URI_USERDB;
 
 export const connectToProjectDatabase = async () => {
   try {
+    
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Connected to database!');
   } catch (err) {
