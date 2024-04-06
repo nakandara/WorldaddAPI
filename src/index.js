388b51ffabd0dfaadb77891b8a120442c70668c6
pramodniroshan <pramodnakandara@gmail.com>
2024-04-07 02:19:01 +0530

    add second port

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080;
 const key = fs.readFileSync('private.key')
 
 const cert = fs.readFileSync('certificate.crt')
- 
 
 const cred = {
   key,
@@ -16,5 +15,9 @@ const cred = {
 }
 
 // Using ES6 arrow function to start the server and log a message
+app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
+
 const httpsServers =  https.createServer(cred,app)
 httpsServers.listen(8443)
