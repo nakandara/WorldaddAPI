77df7de6d41a9717c07fc8f61a6b87a9a564cda3
pramodniroshan <pramodnakandara@gmail.com>
2024-04-07 02:01:58 +0530

    remove port

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import app from './app.js';
 const port = process.env.PORT || 8080;
 
 // Using ES6 arrow function to start the server and log a message
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Listening: http://localhost:${port}`);
+// });
