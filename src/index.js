cb555796306f811383403b75f077ae0e56f09f1a
pramodniroshan <pramodnakandara@gmail.com>
2024-05-18 20:38:48 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 // Using ES6 arrow function to start the server and log a message
 app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening new: http://localhost:${port}`);
 });
 
 
