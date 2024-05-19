1fe4b1cfe5b212105b7781f9362ccc53d719ebfc
pramodniroshan <pramodnakandara@gmail.com>
2024-05-19 20:06:42 +0530

    add s3new

--- src/middleware/uploadConfig.js
+++ src/middleware/uploadConfig.js
@@ -16,7 +16,7 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3,
-    bucket:process.env.AWS_BUCKET,
+    bucket:process.env.S3_BUCKET,
     metadata: (req, file, cb) => {
       cb(null, { fieldName: file.fieldname });
     },
