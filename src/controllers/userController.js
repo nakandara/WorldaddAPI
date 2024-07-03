12fa8840ddc7ad397fe076734c704abbde18ab81
pramodniroshan <pramodnakandara@gmail.com>
2024-07-03 22:11:31 +0530

    change app pas key

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "quickadshub@gmail.com",
-    pass: "Hub",
+    pass: "iosb ikgu jkyj gmjm",
   },
 });
 
