3e20f75d76d1f9a9b0f277ff638e439e49c9a858
pramodniroshan <pramodnakandara@gmail.com>
2024-07-03 23:54:40 +0530

    change directry

--- src/app.js
+++ src/app.js
@@ -16,17 +16,27 @@ import authRoute from './routes/authRoute.js';
 import { connectToProjectDatabase } from './database/projectdb.js';
 import session from 'express-session';
 import { authenticateJWT } from './common/passport.js';
+import path from 'path';
 
 const app = express();
 
+// Create __dirname for ES Module
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Set the view engine to EJS and the views directory
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(
   session({
     secret: 'your-secret-key',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
   })
 );
 
@@ -71,7 +81,6 @@ async function startServer() {
 
 startServer();
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authenticateJWT);
 app.use('/api', UserRoutes);
@@ -82,15 +91,8 @@ app.use('/api', postRoutes);
 app.use('/api', otpRouter);
 app.use('/auth', authRoute);
 
-
-
-
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
+  res.render('index'); // Ensure you have views/index.ejs file
 });
-
-
 
 export default app;
